9e1c5edb79474d7d053b57452481d354b5d8d55a
mturle <m.turlejski@datawise.pl>
2016-05-18 10:03:25 +0200

    Added the function body v1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,78 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Programmatic Assignement 3
 
-## Write a short comment describing this function
+# This function returns a list containing a function to store (set), read (get)
+# the value of a matrix and later to store the the inverted matrix or retrieve it
+# from cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    minv <- NULL
+    set <- function(y) {
+        x <<- y
+        minv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) minv <<- inverse
+    getinverse <- function() minv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+    
 }
 
 
-## Write a short comment describing this function
+# This function returns an iverted matrix via SOLVE function, but first
+# checks wheter an inverted matrix is not already stored in cache. If
+# yes, the inverted matrix is read and not calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    minv <- x$getinverse()
+    if(!is.null(minv)) {
+        message("getting cached data.")
+        return(minv)
+    }
+    data <- x$get()
+    minv <- solve(data)
+    x$setinverse(minv)
+    minv
 }
+
+# Test
+
+# A simple matrix x (3x3)
+# > x <- matrix(c(1,3,3,1,4,3,1,3,4), nrow=3, ncol=3)
+
+# Storing the matrix
+## > m <- makeCacheMatrix(x)
+
+# Retrieve the original matrix from m variable
+## > m$get()
+
+#       [,1] [,2] [,3]
+# [1,]    1    1    1
+# [2,]    3    4    3
+# [3,]    3    3    4
+
+# Test the cacheSolve function for the first run (the inverse of a matrix 
+# is calculated)
+
+# > cacheSolve(m)
+#       [,1]          [,2] [,3]
+# [1,]    7 -1.000000e+00   -1
+# [2,]   -3  1.000000e+00    0
+# [3,]   -3  1.665335e-16    1
+
+# > cacheSolve(m)
+
+# Test the cacheSolve function for the second run (the inverse of a matrix 
+# is read from cache)
+
+# > cacheSolve(m)
+
+# > getting cached data.
+# >      [,1]          [,2] [,3]
+# > [1,]    7 -1.000000e+00   -1
+# > [2,]   -3  1.000000e+00    0
+# > [3,]   -3  1.665335e-16    1
+
+
+ 
